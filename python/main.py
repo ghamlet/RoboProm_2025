50aa774f404d951d3b265f8f38d93a0b7d962995
Pavel <rainbow343@yandex.ru>
2025-04-10 21:08:11 +0300

    add full server logic (camera logic need)

--- python/main.py
+++ python/main.py
@@ -5,29 +5,12 @@ import re
 import time
 
 
-def get_start_arguments(message):
-    args_input = re.fullmatch(r"start:(\d+):(\d{1})#", message)
-    iteration = args_input.group(1)
-    object_is_found = args_input.group(2)
-    return iteration, object_is_found
-
-
-def is_start_msg(message: str) -> bool:
-    return bool(re.fullmatch(r"start:(\d+):(1|3)#", message))
-
-
-def is_sonar_data(message: str) -> bool:
-    data = re.fullmatch(r"(\d+)", message)
-    if data is not None:
-        if int(data.group(1)) > 0:
-            return True
-    return False
-
-
 # Конфигурация сети
-MAIN_IP = "192.168.42.120"  # nanopi
 SERVER_CONTROL_IP = "192.168.42.22"
+
 MAIN_PORT = 3163
+
+MAIN_IP = "192.168.42.120"  # nanopi ip
 
 LAMP_IP = "192.168.42.129"
 LAMP_PORT = 8000
@@ -35,24 +18,40 @@ LAMP_PORT = 8000
 SONAR_IP = "192.168.42.128"
 SONAR_PORT = 8888
 
-# айпи общего сервера разрабов
 LOCAL_IP = "192.168.42.120"
 LOCAL_PORT = 8888
 
+TEAM_NUMBER = 3  # номер команды (для отправки результата на общий сервер)
 
-# Для нормального считывания данных с сонара
-MEASUREMENT_COUNT = 10  # количество замеров от сонара для анализа
-DETECTION_THRESHOLD = (
-    7  # порог для определения большинства (если >= 3 из 5  - объект есть)
-)
-MAX_FAIL_SONAR = MEASUREMENT_COUNT - DETECTION_THRESHOLD
 MAX_DIST_TO_OBJECT = 15  # максимальная длина до детали
+
+
+def get_start_arguments(message):
+    args_input = re.fullmatch(r"start:(\d+):(\d{1})#", message)
+    iteration = args_input.group(1)
+    object_is_found = args_input.group(2)
+    return iteration, object_is_found
+
+
+def is_start_msg(addr: str, message: str) -> bool:
+    data = re.fullmatch(r"start:(\d+):(1|3)#", message)
+    if data is not None and addr == SERVER_CONTROL_IP:
+        return True
+    return False
+
+
+def is_sonar_data(addr: str, message: str) -> bool:
+    data = re.fullmatch(r"(\d+)", message)
+    if data is not None:
+        if int(data.group(1)) > 0 and addr == SONAR_IP:
+            return True
+    return False
+
 
 server = NanoPi(MAIN_IP, MAIN_PORT, LOCAL_IP, LOCAL_PORT)
 server.start_global()
 server.start_local()
 signal_lamp = Lamp(LAMP_IP, LAMP_PORT)
-
 
 signal_lamp.off()
 signal_lamp.test_lamp()
@@ -71,7 +70,7 @@ while True:
         message = data.decode("utf-8").strip()
         print(f"Received from {addr}: {message}")
 
-        if is_start_msg(message):
+        if is_start_msg(addr, message):
             print("Получено стартовое сообщение")
             iteration, obj_is_found = get_start_arguments(message)
             print(f"{iteration=} {obj_is_found=}")
@@ -87,7 +86,7 @@ while True:
                     message = data.decode("utf-8").strip()
                     print(f"Received from {addr}: {message}")
 
-                    if is_sonar_data(message):
+                    if is_sonar_data(addr, message):
                         sonar_data = int(message)
                         print("Сонар: ", sonar_data)
 
@@ -95,21 +94,44 @@ while True:
                             OBJECT_DETECTED = True
 
                         if OBJECT_DETECTED:
-                            signal_lamp.defect()
-                            server.send_final_mesage(
-                                iteration,
-                                3,
-                                2,
-                                SERVER_CONTROL_IP,
-                                MAIN_PORT,
-                            )
-                            break
+                            camera_detection = camera_detection()
+                            if camera_detection == 2:
+                                signal_lamp.defect()
+                                server.send_final_mesage(
+                                    iteration,
+                                    TEAM_NUMBER,
+                                    2,
+                                    SERVER_CONTROL_IP,
+                                    MAIN_PORT,
+                                )
+                                break
+
+                            elif camera_detection == 3:
+                                signal_lamp.delicate()
+                                server.send_final_mesage(
+                                    iteration,
+                                    TEAM_NUMBER,
+                                    3,
+                                    SERVER_CONTROL_IP,
+                                    MAIN_PORT,
+                                )
+                                break
+                            elif camera_detection == 4:
+                                signal_lamp.not_delicate()
+                                server.send_final_mesage(
+                                    iteration,
+                                    TEAM_NUMBER,
+                                    4,
+                                    SERVER_CONTROL_IP,
+                                    MAIN_PORT,
+                                )
+                                break
 
                         else:
                             signal_lamp.not_object()
                             server.send_final_mesage(
                                 iteration,
-                                3,
+                                TEAM_NUMBER,
                                 1,
                                 SERVER_CONTROL_IP,
                                 MAIN_PORT,
